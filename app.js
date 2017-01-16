b28f91516d66042b70466cc2fc60ab6276850faf
Ahmet <asivri@fredonia.edu>
2017-01-16 15:55:01 +0300

    Done with module configuration

---
+++ app.js
@@ -0,0 +1,103 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+//Initializations done by me
+var expressSession= require('express-session');
+var expressValidator = require('express-validator');
+var passport= require('passport');
+var localStrategy= require('passport-local').Strategy;
+var imageUpload= require('multer');
+var upload = imageUpload({dest:'./uploads'});
+var flash= require('connect-flash');
+var database = require('mongodb');
+var databaseConnector = require('mongoose');
+var db = databaseConnector.connection;
+//-----------------------------------------
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Router Portion
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//Created by coder not express itself
+
+//1-)Image Upload(Can also be other types of file)
+//Moved the initialization part
+
+
+//2-)Session
+app.use(expressSession({
+  secret: 'secret key',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+//3-)Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//4-)Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//5-)Express Messages
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+module.exports = app;
